7def61f459a45059bc5347a457603fae7ea12e62
Owen Silver <o.silver@student.unsw.edu.au>
2020-06-18 22:49:35 +1000

    Connected to angular materials, bootstrapped login component

--- frontend/MealMatch-Webapp/src/app/app.module.ts
+++ frontend/MealMatch-Webapp/src/app/app.module.ts
@@ -7,18 +7,55 @@ import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {  MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule  } from '@angular/material/table';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatInputModule} from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { NavbarComponent } from './navbar/navbar.component';
+import { MatOptionModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+const materialModules = [
+  MatToolbarModule,
+  MatToolbarModule,
+  MatMenuModule,
+  MatIconModule,
+  MatButtonModule,
+  MatTableModule,
+  MatDividerModule,
+  MatProgressSpinnerModule,
+  MatInputModule,
+  MatCardModule,
+  MatSlideToggleModule,
+  MatSelectModule,
+  MatOptionModule
+]
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
     LoginComponent,
     SignupComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    BrowserAnimationsModule,
+    materialModules,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
