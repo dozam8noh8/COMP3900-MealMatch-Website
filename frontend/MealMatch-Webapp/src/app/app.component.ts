3b186d2ad0e35f110d68068bda065d28393d62e9
Owen Silver <o.silver@student.unsw.edu.au>
2020-06-18 19:30:30 +1000

    Post demo commit added buttons and cleaned up some files

--- frontend/MealMatch-Webapp/src/app/app.component.ts
+++ frontend/MealMatch-Webapp/src/app/app.component.ts
@@ -4,7 +4,13 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   styleUrls: ['./app.component.scss'],
   template: `
+  <nav>
+  <a routerLink="/home"> Home </a>
+  <a routerLink="/login"> Login </a>
+  <a routerLink="/signup"> Sign Up </a>
+
   
+  </nav>
   <router-outlet></router-outlet>
   `
 })
--- frontend/MealMatch-Webapp/src/app/login/login.component.ts
+++ frontend/MealMatch-Webapp/src/app/login/login.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
-  styleUrls: ['./login.component.scss'],
   template: `<p>This is the login page</p>`
 })
 export class LoginComponent implements OnInit {
