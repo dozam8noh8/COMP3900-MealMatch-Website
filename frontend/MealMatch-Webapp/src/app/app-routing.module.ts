ac14c8b390ecea17a3d176ed7edc5210dbd2cecd
Owen Silver <osilver@freelancer.com>
2020-06-19 10:32:15 +1000

    Added signup component UI

--- frontend/MealMatch-Webapp/src/app/app-routing.module.ts
+++ frontend/MealMatch-Webapp/src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { SignupComponent } from './signup/signup.component';
 
 
 const routes: Routes = [
-  {path: '', pathMatch: 'full', redirectTo: 'home'}, // This redirects to the home route (the next one)
+  //{path: '', pathMatch: 'full', redirectTo: 'home'}, // This redirects to the home route (the next one)
   {path: 'home', component: HomePageComponent}, // this serves the home component
   {path: 'login', component: LoginComponent}, // Serves Login component at /login
   {path: 'signup', component: SignupComponent},
@@ -14,7 +14,7 @@ const routes: Routes = [
 // This is the default "wildcard" if none of the above patterns match, we redirect to '' (home)
 // We could also put a "pageNotFound" component here if we didnt want to confuse
 // the users by redirecting here.
-  {path: '**', redirectTo: 'home'}, 
+  //{path: '**', redirectTo: 'home'},
 ];
 
 @NgModule({
