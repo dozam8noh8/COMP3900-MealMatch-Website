66276f1d76c4499110ccbeddef21c2f9bae5a066
Waqif Macbook Air <z5188982@ad.unsw.edu.au>
2020-06-25 11:46:07 +1000

    Added recipes to data seeding

--- backend/app/seed.py
+++ backend/app/seed.py
@@ -62,5 +62,34 @@ def seed_db():
 
     # print('mealTypes: ', all_mealtypes)
 
+    ########################################################################################################################
+
+    ########################################### SETUP RECIPES ##############################################################
+
+    # Load json
+    input_file=open('data_seed/recipes.json', 'r')
+    json_decode=json.load(input_file)
+
+    user = User(username='admin', password_hash='pbkdf2:sha256:150000$V5gA5nPN$3377ab719495472c4b4f6efcdb0066d7591c29f3f5721dcb469ddd5c54fb9232')
+    db.session.add(user)
+    db.session.commit()
+
+    for item in json_decode['meals']:
+        # Make new recipe
+        recipe = Recipe(name=item['name'],instruction=item['instruction'])
+        db.session.add(recipe)
+        db.session.commit()
+
+        mealtype = Mealtype.query.filter_by(name=item['mealtype']).first()
+        recipe.mealtypes.append(mealtype)
+        db.session.commit()
+
+        ingredients = item['ingredients']
+        for ingredient in ingredients:
+            db_ingredient = Ingredient.query.filter_by(name=ingredient['name']).first()
+            if db_ingredient:
+                recipe.ingredients.append(db_ingredient)
+        user.recipes.append(recipe)
+        db.session.commit()
 
     ########################################################################################################################
