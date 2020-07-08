d0cbf7b615db5d6be629b140f6079d315989ead5
Manni <manni.huang18@gmail.com>
2020-07-08 19:00:03 +1000

    Search results are maintained after pressing back from a recipe page

--- frontend/MealMatch-Webapp/src/app/search-results/search-results.component.ts
+++ frontend/MealMatch-Webapp/src/app/search-results/search-results.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Recipe } from '../models/recipe';
-import { HttpClient } from '@angular/common/http';
+import { SearchService } from '../services/search.service';
 
 @Component({
   selector: 'app-search-results',
@@ -10,24 +10,27 @@ import { HttpClient } from '@angular/common/http';
 })
 export class SearchResultsComponent implements OnInit {
 
-  searchState;
-  resultingRecipes: Recipe[];
-  searchComplete = false;
-
-  constructor(private router: Router, private http: HttpClient) {
-    this.searchState = this.router.getCurrentNavigation().extras.state;
+  constructor(private router: Router, private searchService: SearchService) {
+    let searchState = this.router.getCurrentNavigation().extras.state;
+    // If a list of ingredients was passed from search (home page)
+    if(searchState) {
+      this.searchService.searchForRecipes(searchState.searchIngredients);
+    }
   }
 
   ngOnInit(): void {
-    // We should change this from just being ngOnInit because there is no search state on page refresh.
+  }
 
-    this.http.post("http://localhost:5000/api/recipe_search", {
-      "ingredients": this.searchState.searchIngredients
-    })
-    .subscribe( (data: Recipe[]) => {
-      this.resultingRecipes = data;
-      this.searchComplete = true;
-    });
+  getResults() {
+    return this.searchService.getAllResults()
+  }
+
+  getSearchedIngredients() {
+    return this.searchService.inputIngredients;
+  }  
+
+  searchComplete() {
+    return this.searchService.searchComplete;
   }
 
 
---
+++ frontend/MealMatch-Webapp/src/app/services/search.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { Ingredient } from '../models/ingredient';
+import { HttpClient } from '@angular/common/http';
+import { Recipe } from '../models/recipe';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SearchService {
+
+  allMealTypes = ['All', 'Breakfast', 'Lunch', 'Dinner', 'Miscellaneous'];
+  
+  searchComplete = false;
+  inputIngredients: Ingredient[];
+  mealType: String;
+  allResults: Recipe[];
+
+  constructor(private http: HttpClient) { }
+
+  searchForRecipes(ingredients: Ingredient[]) {
+    this.inputIngredients = ingredients;
+    this.http.post("http://localhost:5000/api/recipe_search", {
+      "ingredients": ingredients
+    })
+    .subscribe( (data: Recipe[]) => {
+      this.allResults = data;
+      this.searchComplete = true;
+    });
+  }
+
+  getAllResults() {
+    return this.allResults;
+  }
+
+
+}
