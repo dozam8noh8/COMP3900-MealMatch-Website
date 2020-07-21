250a83f5715e7eeaf8c55bcad5157d542abd4c61
Owen Silver <osilver@freelancer.com>
2020-07-22 08:49:13 +1000

    Add guard to recipe edit page so only owners can edit

--- frontend/MealMatch-Webapp/src/app/app-routing.module.ts
+++ frontend/MealMatch-Webapp/src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ import { AuthGuardService } from './auth/auth-guard.service';
 import { CreateRecipeComponent } from './create-recipe/create-recipe.component';
 import { LovelessSetsComponent } from './loveless-sets/loveless-sets.component';
 import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+import { RecipeEditGuardService } from './recipe-edit/recipe-edit-guard.service';
 
 
 const routes: Routes = [
@@ -20,7 +21,7 @@ const routes: Routes = [
   {path: 'recipe/:id', component: RecipeInfoComponent},
   {path: 'search', component: SearchResultsComponent},
   {path: 'create', component: CreateRecipeComponent, canActivate: [AuthGuardService]},
-  {path: 'edit/:id', component: RecipeEditComponent, canActivate: [AuthGuardService]},
+  {path: 'edit/:id', component: RecipeEditComponent, canActivate: [AuthGuardService, RecipeEditGuardService]},
   {path: 'lovelesssets', component: LovelessSetsComponent}, // Component for sets with no recipes
   {path: 'dashboard', component: ProfilePageComponent, canActivate: [AuthGuardService]}, // This is just to demonstrate a resource only logged in users can access.
 // This is the default "wildcard" if none of the above patterns match, we redirect to '' (home)
---
+++ frontend/MealMatch-Webapp/src/app/recipe-edit/recipe-edit-guard.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { RecipeService } from '../services/recipe.service';
+import { AuthService } from '../services/auth.service';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipeEditGuardService implements CanActivate {
+
+  constructor(private recipeService: RecipeService, private router: Router, private authService: AuthService) { }
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> {
+    let recipeId = route.params?.id
+    // If user has typed a route like /edit/glkjfg, redirect to notfound
+    if (!recipeId) {
+      this.router.navigate(['/notfound'])
+      return false;
+    }
+    let userId = this.authService.getLoggedInUserId()
+    return this.recipeService.getRecipeDetails(recipeId).pipe(map((res: any) => {
+      if (res.user_id === userId) {
+        return true;
+      }
+      else {
+        return false;
+      }
+    }));
+
+  }
+}
