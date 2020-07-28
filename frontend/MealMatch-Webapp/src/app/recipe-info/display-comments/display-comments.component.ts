2a887c1cce7446e4967a3068c428154479ee3065
Manni <manni.huang18@gmail.com>
2020-07-28 23:45:56 +1000

    Logic for when there is no RC yet

--- frontend/MealMatch-Webapp/src/app/recipe-info/display-comments/display-comments.component.ts
+++ frontend/MealMatch-Webapp/src/app/recipe-info/display-comments/display-comments.component.ts
@@ -9,12 +9,47 @@ import { FormGroup, FormControl } from '@angular/forms';
   selector: 'app-display-comments',
   styleUrls: ['./display-comments.component.scss'],
   template: `
-              <h3>Reviews</h3>
+              <h2>Reviews</h2>
+              <mat-spinner *ngIf="loadingComments"> </mat-spinner> 
+
+              <ng-container *ngIf="!loadingComments && currentUser && !existingRC">
+                <mat-spinner *ngIf="postingUserRC"> </mat-spinner> 
+                <form 
+                *ngIf="!postingUserRC"
+                [formGroup]="ratingCommentFormGroup" 
+                (ngSubmit)="postRatingComment()">
+                  <ngb-rating 
+                  [formControl]="ratingCommentFormGroup.get('rating')"
+                  [max]="5"
+                  [readonly]="false">
+                    <ng-template let-fill="fill" let-index="index">
+                      <span class="star" [class.filled]="fill === 100">&#9733;</span>
+                    </ng-template>
+                  </ngb-rating>
+
+                  <div>
+                    Comment: <br/>
+                    <textarea formControlName="comment"></textarea>
+                    <br/>
+                    <button type="submit" mat-raised-button color="primary">Add a review</button>            
+                  </div>
+                </form>
+              </ng-container>
+
+              <ng-container *ngIf="allRatingComments && allRatingComments.length <= 0">
+                There are no reviews for this recipe.
+              </ng-container>
+
               <ng-container *ngFor="let rc of allRatingComments">
 
-                <div *ngIf="currentUser && rc.username === currentUser['username'] && editable; else elseNotEditable">
+                <div class="rating-comment" *ngIf="currentUser && rc.username === currentUser['username'] && editable; else elseNotEditable">
+                  <mat-spinner *ngIf="postingUserRC"> </mat-spinner> 
+
                   <!-- Allow the user to change rating and comment -->
-                  <form [formGroup]="ratingCommentFormGroup" (ngSubmit)="postRatingComment()" class="rating-comment">
+                  <form 
+                  *ngIf="!postingUserRC"
+                  [formGroup]="ratingCommentFormGroup" 
+                  (ngSubmit)="postRatingComment()">
                     <ngb-rating 
                     [formControl]="ratingCommentFormGroup.get('rating')"
                     [max]="5"
@@ -48,8 +83,8 @@ import { FormGroup, FormControl } from '@angular/forms';
                     </ngb-rating>
 
                     <!-- If a user is logged in and the ratingComment belongs to this user -->
-                    <span *ngIf="currentUser && rc.username === currentUser['username']">
-                      Your comment and rating <button mat-raised-button color="primary" (click)="toggleEdit()">Edit</button>
+                    <span *ngIf="currentUser && rc.username === currentUser['username'] && !postingUserRC">
+                      This is your comment and rating <button mat-raised-button color="primary" (click)="toggleEdit()">Edit</button>
                     </span>
 
                     <span *ngIf="!currentUser || rc.username !== currentUser['username']">
@@ -76,6 +111,10 @@ export class DisplayCommentsComponent implements OnInit {
   ratingCommentFormGroup: FormGroup;
   ratingControl: FormControl;
   editable = false;
+  existingRC: RatingComment;
+
+  loadingComments: boolean;
+  postingUserRC: boolean;
 
   constructor(
     private rcService: RatingCommentService,
@@ -91,7 +130,26 @@ export class DisplayCommentsComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.loadingComments = true;
+    this.getAllRatingComments();
+  }
 
+  postRatingComment() {
+    this.postingUserRC = true;
+    let rating = this.ratingCommentFormGroup.get('rating').value;
+    let comment = this.ratingCommentFormGroup.get('comment').value;
+    this.rcService.postRatingComment(this.recipeId, rating, comment)
+    .subscribe( (resp) => {
+      console.log(resp);
+      this.editable = false;
+      this.getAllRatingComments();
+    },
+    err => {
+      console.log("ERROR")
+    })
+  }
+
+  getAllRatingComments() {
     this.rcService.getAllRatingComments(this.recipeId)
     .subscribe( (rcResp: RatingComment[]) => {
 
@@ -99,49 +157,41 @@ export class DisplayCommentsComponent implements OnInit {
       this.authService.getUserDetails()
       .subscribe( (userResp) => {
 
-        this.currentUser = userResp;
+        this.currentUser = userResp;   
 
-        // Sort all ratingComments so that if there is one by the user, it is first
         this.allRatingComments = rcResp;
-        this.allRatingComments = this.allRatingComments.sort( rc => {
-          if(rc.username === this.currentUser['username']) {
-            // If is a rating by the user already, set the form as well
-            this.ratingCommentFormGroup.get('rating').setValue(rc.rating);
-            this.ratingCommentFormGroup.get('comment').setValue(rc.comment);
-            return -1;
-          }
-          return 0;
-        })
+        this.existingRC = this.allRatingComments.find( rc => rc.username === this.currentUser['username'] );
 
+        // If there is already a ratingComment by the user
+        if(this.existingRC) {
+          // Remove it at its index
+          const index = this.allRatingComments.findIndex( elem => elem===this.existingRC);
+          this.allRatingComments.splice(index, 1);
 
+          // Place it at the front
+          this.allRatingComments.unshift(this.existingRC);
+          
+          // Set the form
+          this.ratingCommentFormGroup.get('rating').setValue(this.existingRC.rating);
+          this.ratingCommentFormGroup.get('comment').setValue(this.existingRC.comment);
+        }
+
+        this.postingUserRC = false;
+        this.loadingComments = false;
       },
       (err) => {
         // If error with API call, it is likely that there is no user logged in
         // so just return all ratingComments
         this.allRatingComments = rcResp;
+        this.loadingComments = false;
       })
     })
 
 
   }
 
-
-
-  postRatingComment() {
-    console.log(this.formRating)
-    console.log(this.formComment)
-  }
-
   toggleEdit() {
     this.editable = !this.editable;
-  }
-
-  get formRating() {
-    return this.ratingCommentFormGroup.get('rating').value;
-  }
-
-  get formComment() {
-    return this.ratingCommentFormGroup.get('comment').value;
   }
 
 }
--- frontend/MealMatch-Webapp/src/app/services/rating-comment.service.ts
+++ frontend/MealMatch-Webapp/src/app/services/rating-comment.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RatingComment } from '../models/rating_comment';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,11 +11,24 @@ export class RatingCommentService {
   private BASE_URL = 'http://localhost:5000/api';
 
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    private authService: AuthService
   ) { }
 
   getAllRatingComments(recipeId: number) {
     let url = `${this.BASE_URL}/rating/${recipeId}`;
     return this.http.get<RatingComment[]>(url);
   }
+
+  postRatingComment(recipeId: number, rating: number, comment: string) {
+    let url = `${this.BASE_URL}/rating/${recipeId}`;
+    let body = {
+      "rating": rating,
+      "comment": comment,
+      "user_id": this.authService.getLoggedInUserId()
+    }
+    console.log(body)
+    return this.http.post(url, body);
+  }
+
 }
